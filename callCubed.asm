929600474129297466ec036b89b91fde933280f1
lst97 <laisiotou1997@gmail.com>
2018-12-07 19:15:24 +1100

    Call to calcuate the cubed from memory

---
+++ callCubed.asm
@@ -0,0 +1,64 @@
+;/*
+;|**********************************************************************;
+;* Project           : misc_Assembly
+;*
+;* Program name      : callCubed.asm
+;*
+;* Author            : SIO TOU LAI (laisiotou1997@gmail.com)
+;*
+;* Date created      : 07/12/2018
+;*
+;* Purpose           : call to calcuate the cubed from memory
+;*
+;* Revision History  :
+;*
+;* Date        Author      Ref    Revision (Date in DDMMYYYY format) 
+;* 07122018    lst97       1      First release
+;*
+;* Known Issue       :
+;*
+;* N/A
+;|**********************************************************************;
+;*/
+
+assume cs:code, ds:data, ss:stack
+code segment
+	
+data segment
+	dw 1, 2, 3, 4, 5, 6, 7, 8
+	dw 8 dup (0)
+data ends
+
+stack segment
+	dw 0
+stack ends
+
+main:	mov ax, data
+	mov ds, ax
+	mov ax, stack
+	mov ss, ax
+	mov sp, 0002h
+	mov bx, 00h
+	mov cx, 0008h
+	mov si, 00h
+	
+lpNum:	mov ax, ds:[si]
+	push bx
+	call cubed
+	pop bx
+	mov ds:[bx + 10h], ax
+	add bx, 02h
+	add si, 02h
+	loop lpNum
+
+	mov ax, 4C00h
+	int 21h
+
+cubed:	mov bx, ax
+	mul bx
+	mul bx
+	ret
+
+
+code ends
+end main
